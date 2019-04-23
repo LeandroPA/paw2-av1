ff4416338465c04a14b2f50dc362261cd8c40488
LeandroPA <leandroalencar@unigranrio.br>
2019-04-22 23:52:25 -0300

    Versão final

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,13 @@ function selectMenu(menuIndex) {
     var elements = document.getElementsByClassName("sub-menu");
 
     for (i = 0; i < elements.length; i++) {
-        elements[i].style.visibility = "hidden"; 
+        elements[i].style.display = "none"; 
     }
 
     var element = document.getElementById(menuIndex);
     if (element)
-        element.style.visibility = "visible";
+        element.style.display = "flex";
 }
+
+//Página principal
+selectMenu(0);
